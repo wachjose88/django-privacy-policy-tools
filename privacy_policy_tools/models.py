cc2c0aaee863cce7a4295d0eb4632a6791bf97c4
Josef Wachtler <josef.wachtler@gmail.com>
2022-12-16 11:35:02 +0100

    add richtextfield

--- privacy_policy_tools/models.py
+++ privacy_policy_tools/models.py
@@ -27,6 +27,7 @@ from django.db import models
 from django.contrib.auth.models import Group, User
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
+from ckeditor.fields import RichTextField
 
 
 class PrivacyPolicy(models.Model):
@@ -43,7 +44,7 @@ class PrivacyPolicy(models.Model):
     """
     title = models.CharField(max_length=128, verbose_name=_('Title'),
                              default=_('Privacy Policy'))
-    text = models.TextField(verbose_name=_('Text'))
+    text = RichTextField(verbose_name=_('Text'))
     active = models.BooleanField(default=False, verbose_name=_('Active'))
     published_at = models.DateTimeField(default=timezone.now,
                                         verbose_name=_('Published at'))
